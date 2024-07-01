48079f26e2863ef0e9c585f6c5d6bb40e82f1a72
Lennox Mountain <lennoxmountain67@gmail.com>
2024-07-01 20:28:55 +0200

    added debouncing feature. solves the problem of firing a db query on every keystroke

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,31 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'; //updates the url with search params 
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const {replace} = useRouter();
+
+  const handleSearch = useDebouncedCallback((term => {
+    const params = new URLSearchParams(searchParams);
+    if (term){
+      params.set('query', term);
+    }else{
+      params.delete('query');
+    }
+
+    //updating the url
+    replace(`${pathname}?${params.toString()}`)
+    /**
+     * CODE BREAK DOWN
+     * the line updates the url with the new search query
+     * the ? separates the path and the search string
+     * e'g /dashboard/invoices?query=lee where lee is a search query
+     */
+  }), 300)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +34,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e)=>{
+        handleSearch(e.target.value);
+         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
